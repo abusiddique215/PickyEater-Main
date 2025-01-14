1ad2d1a7aa301f4779b24ee105fb39da87d2db8a
abusiddique215 <95459462+abusiddique215@users.noreply.github.com>
2025-01-14 01:57:38 -0500

    q

---
+++ PickyEater2/Core/Sources/PickyEater2Core/Models/DietaryRestriction.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public enum DietaryRestriction: String, Codable, CaseIterable {
+    case dairyFree = "Dairy-Free"
+    case nutFree = "Nut-Free"
+    case halal = "Halal"
+    case kosher = "Kosher"
+    
+    public var id: String { rawValue }
+    public var description: String { rawValue }
+} 
