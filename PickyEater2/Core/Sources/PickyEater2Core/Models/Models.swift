f72066898a38804256a37fb2df2469972e272fca
abusiddique215 <95459462+abusiddique215@users.noreply.github.com>
2025-01-14 01:57:32 -0500

    q

--- PickyEater2/Core/Sources/PickyEater2Core/Models/Models.swift
+++ PickyEater2/Core/Sources/PickyEater2Core/Models/Models.swift
@@ -1,15 +1,5 @@
 import Foundation
 
-public struct Category: Codable, Equatable {
-    public let alias: String
-    public let title: String
-    
-    public init(alias: String, title: String) {
-        self.alias = alias
-        self.title = title
-    }
-}
-
 public struct RestaurantSearchResponse: Codable {
     public let restaurants: [AppRestaurant]
     
