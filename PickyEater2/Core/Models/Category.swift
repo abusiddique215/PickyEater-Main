29d5fe3727064af0e28c4516349e3e7eab16c40f
abusiddique215 <95459462+abusiddique215@users.noreply.github.com>
2025-01-14 01:48:05 -0500

    q

---
+++ PickyEater2/Core/Models/Category.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public struct Category: Codable, Equatable {
+    public let alias: String
+    public let title: String
+    
+    public init(alias: String, title: String) {
+        self.alias = alias
+        self.title = title
+    }
+} 
