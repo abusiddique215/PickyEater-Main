6b4c4f980dc0bb96e41471915443b8f9382083e4
abusiddique215 <95459462+abusiddique215@users.noreply.github.com>
2025-01-14 02:12:00 -0500

    q

---
+++ Sources/PickyEater2Core/Models/AppTheme.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+public enum AppTheme: String, CaseIterable {
+    case light = "Light"
+    case dark = "Dark"
+    case system = "System"
+    
+    public var colorScheme: ColorScheme? {
+        switch self {
+        case .light:
+            return .light
+        case .dark:
+            return .dark
+        case .system:
+            return nil
+        }
+    }
+} 
